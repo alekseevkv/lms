9f7bfb3af40d3e6b727a5a6ad22c80b183a7f90d
Konstantin Alekseev <240989073+alekseevkv@users.noreply.github.com>
2025-11-23 16:33:01 +0300

    Change db url

--- alembic/env.py
+++ alembic/env.py
@@ -9,12 +9,13 @@ from alembic import context
 from src.configs.app import settings
 from src.models.base import Base
 from src.models.course import Course  # noqa: F401
+from src.models.user import User  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
-config.set_main_option("sqlalchemy.url", settings.db.dsl)
+config.set_main_option("sqlalchemy.url", settings.db.dsl.replace("@db:", "@localhost:"))
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
